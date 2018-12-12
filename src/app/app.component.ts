436b9cb8bad8daf38f69a7bec819f58e002244c8
Mikhail <mech712@gmail.com>
2018-12-12 16:31:42 +0500

    main module and component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { CountService } from './count.service';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers:[CountService]
 })
 export class AppComponent {
   title = 'inf';
